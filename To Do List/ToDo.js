5150c34ce1496d8670dce54af9fb05934b28090c
Dennis Gallina <116260248+dennisgallina@users.noreply.github.com>
2023-04-03 21:12:58 +0200

    Add files via upload

--- To Do List/ToDo.js
+++ To Do List/ToDo.js
@@ -10,6 +10,13 @@ class ToDo {
         this.data = data;
     }
 
+    seScaduta() {
+        const oggi = new Date();
+
+        let differenzaMillisecondi = Math.abs(this.data - oggi);
+        let differenzaGiorni = Math.ceil(differenzaMillisecondi / (1000 * 60 * 60 * 24));
+    }
+
     getData() {
         return (this.data.getDate() + "-" + (this.data.getMonth() + 1) + "-" + this.data.getFullYear());
     }
