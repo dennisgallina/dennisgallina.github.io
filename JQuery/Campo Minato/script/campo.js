3be5d456c8e344471a70be2e0ac1ab4db596b314
Dennis Gallina <116260248+dennisgallina@users.noreply.github.com>
2023-04-26 21:59:30 +0200

    Add files via upload

---
+++ JQuery/Campo Minato/script/campo.js
@@ -0,0 +1,13 @@
+class Campo {
+    constructor() {
+        this.caselle = [];
+    }
+
+    genera() {
+        for (let i = 0, j = 0; i < 9; i++, j++) {
+            if (j == 9) {
+
+            }
+        }
+    }
+}
